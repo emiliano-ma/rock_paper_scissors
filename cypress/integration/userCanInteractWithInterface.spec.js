5c713998ea900def7ab9b797d86b30b9e64a0cd8
emiliano-ma <emilianomainero@gmail.com>
2020-09-17 21:23:04 +0200

    Setting app react app with cypress, jest and enzyme as testing framework/testing helpers. Cleaning up default react and adding folder __test__ folder for component test

---
+++ cypress/integration/userCanInteractWithInterface.spec.js
@@ -0,0 +1,11 @@
+// describe 'user can interact with the interface' 
+// before do
+// visit root
+// get(start-game)
+// get rock-button click
+
+// it expect to see random computers choice
+// rock paper scissors
+
+// it expect to see result
+// you 
