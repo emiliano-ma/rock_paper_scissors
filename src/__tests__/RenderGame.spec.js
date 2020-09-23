abe2e6117200e30f3caac3945132c9c75f476174
emiliano-ma <emilianomainero@gmail.com>
2020-09-23 08:57:32 +0200

    Adding RenderGame.spec all tests going green

---
+++ src/__tests__/RenderGame.spec.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import RenderGame from "../components/RenderGame";
+
+describe("RenderGame", () => {
+  const selectionHandler = jest.fn();
+  const onClickHandler = jest.fn();
+  const wrapper = shallow(
+    <RenderGame
+      playerName="Emiliano"
+      selectionHandler={selectionHandler}
+      onClickHandler={onClickHandler}
+    />
+  );
+  // .text()).toEqual(
+  it("renders with playerName prop", () => {
+    expect(wrapper.find("#player-name").text()).toEqual("Best of luck Emiliano!");
+  });
+
+
+  it("on click the selectionHandler is being called", () => {
+    wrapper.find("#rock-").simulate("click");
+    expect(selectionHandler).toHaveBeenCalled();
+    wrapper.find("#paper-").simulate("click");
+    expect(selectionHandler).toHaveBeenCalled();
+    wrapper.find("#scissors-").simulate("click");
+    expect(selectionHandler).toHaveBeenCalled();
+  });
+
+  it("on click the onClickHandler is being called", () => {
+    wrapper.find("#back").simulate("click");
+    expect(onClickHandler).toHaveBeenCalled();
+  });
+});
--- src/components/RenderGame.jsx
+++ src/components/RenderGame.jsx
@@ -4,9 +4,9 @@ import { Button } from 'semantic-ui-react'
 const RenderGame = ({selectionHandler, onClickHandler, playerName}) => {
   return (
     <>
-      <button onClick={selectionHandler} id="rock"><span id="rock" aria-label="jsx-a11y/accessible-emo" role="img">✊</span></button>
-      <button onClick={selectionHandler} id="paper" name="Paper"><span id="paper" aria-label="jsx-a11y/accessible-emo" role="img">✋</span></button>
-      <button onClick={selectionHandler} id="scissors" name="Scissors"><span id="scissors" aria-label="jsx-a11y/accessible-emo" role="img">✌</span></button>
+      <button onClick={selectionHandler} id="rock-"><span id="rock" aria-label="jsx-a11y/accessible-emo" role="img">✊</span></button>
+      <button onClick={selectionHandler} id="paper-" name="Paper"><span id="paper" aria-label="jsx-a11y/accessible-emo" role="img">✋</span></button>
+      <button onClick={selectionHandler} id="scissors-" name="Scissors"><span id="scissors" aria-label="jsx-a11y/accessible-emo" role="img">✌</span></button>
       <Button id="back" onClick={onClickHandler}>Back to edit Name</Button>
       <p id="player-name">Best of luck {playerName}!</p>
       <br/>
